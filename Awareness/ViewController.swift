a9777cc5a7f276e0b0cf5ebdff1254c97f0e1500
Josh Dorland <joshdorland@icloud.com>
2022-01-09 10:17:45 -0500

    Update to programmatic UI

--- Awareness/ViewController.swift
+++ Awareness/ViewController.swift
@@ -12,6 +12,17 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        view.backgroundColor = UIColor(red: 1.0, green: 0.95, blue: 0.74, alpha: 1.0)
+        
+        let title = UILabel(frame: CGRect(x: 0, y: 0, width: 200, height: 21))
+        title.text = "Awareness"
+        title.textColor = .black
+        
+        let titletwo = UILabel(frame: CGRect(x: 0, y: 0, width: 200, height: 21))
+        titletwo.backgroundColor = .white
+
+        self.view.addSubview(titletwo)
+        self.view.addSubview(title)
     }
 
 
---
+++ Awareness/constants.swift
@@ -0,0 +1,8 @@
+//
+//  constants.swift
+//  Awareness
+//
+//  Created by Josh Dorland on 2022-01-09.
+//
+
+import Foundation
